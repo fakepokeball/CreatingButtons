85e4e9f021f970f76e12d702a3f16eab63d65854
cowan <cowan@DESKTOP-E1DT78U>
2022-07-21 11:14:02 +0300

    Add project files.

---
+++ RuntimeControlOlusturma/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RuntimeControlOlusturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(54, 122);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(75, 23);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "Buton Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 245);
+            this.Controls.Add(this.btnUret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+    }
+}
+
---
+++ RuntimeControlOlusturma/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RuntimeControlOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayac = 1;
+        Dictionary<int, int> xyler = new Dictionary<int, int>();
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Width = 50;
+            btn.Height = 20;
+
+            btn.Text = sayac.ToString();
+            //btn.BringToFront();
+
+
+
+            int donguSayisi = 0;
+            int x=0, y=0;
+            Random rnd = new Random();
+            bool uygunMu = false;
+
+            while (uygunMu == false)
+            {
+                do
+                {
+
+                x = rnd.Next(0, this.ClientSize.Width - btn.Width);
+                }
+                while(xyler.ContainsKey(x));
+
+                do
+                {
+                y = rnd.Next(0, this.ClientSize.Height - btn.Height);
+                }
+                while (xyler.ContainsKey(y));
+
+                uygunMu = true;
+
+                foreach (var item in xyler)
+                {
+                    if (((x < item.Key && x + 50 > item.Key) || (x > item.Key && x < item.Key + 50) || (x == item.Key)) && ((y < item.Value && y + 20 > item.Value) || (y > item.Value && y < item.Value + 20) || (y == item.Value)))
+                    {
+                        donguSayisi++;
+                        uygunMu = false;
+                        break;
+                    }
+
+
+                }
+                if(donguSayisi > 500000)
+                {
+                    MessageBox.Show("yer kalmadı");
+                    break;
+                }
+
+            }
+
+
+            btn.Left = x;
+            btn.Top = y;
+
+            btn.Click += Btn_Click;
+
+            btn.MouseHover += Btn_MouseHover;
+
+            btn.MouseLeave += Btn_MouseLeave;
+
+            if (uygunMu)
+            {
+                xyler.Add(x, y);
+
+                sayac++;
+
+
+                this.Controls.Add(btn);
+            }
+
+        }
+
+        private void Btn_MouseLeave(object sender, EventArgs e)
+        {
+            Button btnTiklanan = (Button)sender;
+            btnTiklanan.BackColor = Color.WhiteSmoke;
+        }
+
+        private void Btn_MouseHover(object sender, EventArgs e)
+        {
+            Button btnTiklanan = (Button)sender;
+            btnTiklanan.BackColor = Color.Red;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btnTiklanan=(Button)sender;
+
+            MessageBox.Show("merhaba" + btnTiklanan.Text);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                if(item is Button)
+                {
+                    xyler.Add(item.Location.X, item.Location.Y);
+                }
+            }
+        }
+    }
+}
